4f4c87b8d119503c914d696ec5e061558d8cd47f
brainfuck <lixiaofan.dev@gmail.com>
2025-04-16 17:36:33 +0800

    fix

--- packages/components/src/utils/new.tsx
+++ packages/components/src/utils/new.tsx
@@ -1,17 +1,17 @@
-import { get } from "@noah-libjs/utils"
+import { get, isEmpty } from "@noah-libjs/utils"
 import { FormInstance } from "antd"
 
 export function handle_form_error(error: any, form?: FormInstance) {
     const err_filed: { errors: string[], name: string[] } = get(error, 'errorFields[0]')
     if (err_filed) {
 
-        const { name, } = err_filed
+        const { name, errors } = err_filed
 
         if (name) {
             form?.scrollToField(name, { behavior: 'smooth', block: 'center' })
         }
 
-        return err_filed
+        return { ...err_filed, text: isEmpty(errors) ? null : errors.join(',') }
 
     }
     return null
