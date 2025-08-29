c73858375f39215bf695aafd0880d8bcf43dcfe4
brainfuck <brainfuck@lian-med.com>
2025-08-29 16:45:33 +0800

    fix

--- packages/components/src/utils/new.tsx
+++ packages/components/src/utils/new.tsx
@@ -1,4 +1,4 @@
-import { get, isEmpty } from "@noah-libjs/utils"
+import { AnyObject, get, isEmpty } from "@noah-libjs/utils"
 import { FormInstance } from "antd"
 
 export function handle_form_error(error: any, form?: FormInstance) {
@@ -15,4 +15,21 @@ export function handle_form_error(error: any, form?: FormInstance) {
 
     }
     return null
+}
+
+export function form_validate<T = AnyObject>(form: FormInstance) {
+    return new Promise<T>((res, rej) => {
+        form
+            .validateFields()
+            .then(() => {
+                const formData = form.getFieldsValue();
+                res(formData)
+
+            })
+            .catch((error) => {
+                const first_err = handle_form_error(error, form)
+                rej(first_err)
+            })
+    })
+
 }
