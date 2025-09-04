1a435c61c6d554481c3b017e624a2d732c75ffc2
brainfuck <brainfuck@lian-med.com>
2025-09-04 17:06:16 +0800

    fix

--- packages/components/src/utils/new.tsx
+++ packages/components/src/utils/new.tsx
@@ -1,5 +1,6 @@
 import { AnyObject, get, isEmpty } from "@noah-libjs/utils"
 import { FormInstance } from "antd"
+import { useRef } from "react"
 
 export function handle_form_error(error: any, form?: FormInstance) {
     const err_filed: { errors: string[], name: string[] } = get(error, 'errorFields[0]')
@@ -36,4 +37,36 @@ export function form_validate<T = AnyObject>(form?: FormInstance | null) {
             })
     })
 
+}
+
+type EFocus = React.FocusEvent<HTMLDivElement, Element>
+export function fuck_focus(props: { onBlur?(e: EFocus): void }) {
+    const { onBlur } = props
+    const forcusInfo = useRef<{ index?: number, type?: 'child' | 'parent' }>({})
+
+
+
+    function child_focus(index = 0) {
+        forcusInfo.current = { index, type: 'child' }
+    }
+    function child_blur(index = 0) {
+        forcusInfo.current = { index: undefined, type: undefined }
+    }
+    function parent_blur(e: EFocus, index = 0) {
+
+        setTimeout(() => {
+            if (forcusInfo.current.index === index && forcusInfo.current.type === 'child')
+                return
+            forcusInfo.current = { index: undefined, type: undefined }
+            onBlur?.(e)
+        }, 10);
+    }
+
+    function parent_focus(index = 0) {
+
+        // setTimeout(() => {
+        //     forcusInfo.current = { index, type: 'parent' }
+        // }, 10);
+    }
+    return { child_blur, child_focus, parent_blur, parent_focus }
 }
