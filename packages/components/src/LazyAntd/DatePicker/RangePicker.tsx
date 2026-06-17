e0a81834a2084bef7e106e2aa8158401a672140b
noah <noah@gmail.com>
2026-06-17 16:58:55 +0800

    feat: add presets_range

--- packages/components/src/LazyAntd/DatePicker/RangePicker.tsx
+++ packages/components/src/LazyAntd/DatePicker/RangePicker.tsx
@@ -3,11 +3,11 @@
 import React from 'react';
 
 import { RangePickerInner, RangePickerProps } from './Base';
-import { getMomentRange } from '@noah-libjs/utils'
+import { presets_range } from '@noah-libjs/utils'
 
 
 const RangePicker_L = React.forwardRef<any, RangePickerProps>((props, ref) => {
-  return <RangePickerInner ranges={getMomentRange()}  {...props} ref={ref} />;
+  return <RangePickerInner presets={presets_range()}  {...props} ref={ref} />;
 });
 
 RangePicker_L.displayName = 'RangePicker';
--- packages/utils/src/moment-help.ts
+++ packages/utils/src/moment-help.ts
@@ -1,5 +1,5 @@
 import dayjs, { ConfigType, OpUnitType, QUnitType } from "dayjs"
-import { isFunction, isString } from "./helper"
+import { isFunction, isString, listify } from "./helper"
 
 
 export type Dayjs = dayjs.Dayjs
@@ -41,22 +41,31 @@ function end(m = dayjs) {
 
 export function getMomentRange(m = dayjs) {
     type DT = [Dayjs, Dayjs]
-
+    const which_quarter = dayjs_quarter(start(m))[1]
+    const quarter_start = dayjs_quarter(start(m).startOf('month'), which_quarter)[0]
+    const quarter_end = quarter_start.add(2, 'month').endOf('month')
     return {
         昨天: [start(m).add(-1, 'day'), end(m).add(-1, 'day')] as DT,
         今天: [start(m), end(m)] as DT,
         明天: [start(m).add(1, 'day'), end(m).add(1, 'day')] as DT,
         上周: [start(m).add(-1, 'week').startOf('week'), end(m).add(-1, 'week').endOf('week')] as DT,
-        // 这周: [start(m).startOf('week'), end(m).endOf('week')] as DT,
+        本周: [start(m).startOf('week'), end(m).endOf('week')] as DT,
         近一周: [start(m).add(-1, 'week').add(1, 'day'), end(m)] as DT,
         下周: [start(m).add(1, 'week').startOf('week'), end(m).add(1, 'week').endOf('week')] as DT,
         上月: [start(m).add(-1, 'month').startOf('month'), end(m).add(-1, 'month').endOf('month')] as DT,
-        // 这月: [start(m).startOf('month'), end(m).endOf('month')] as DT,
+        本月: [start(m).startOf('month'), end(m).endOf('month')] as DT,
         近一月: [start(m).add(-1, 'month').add(1, 'day'), end(m)] as DT,
         下月: [start(m).add(1, 'month').startOf('month'), end(m).add(1, 'month').endOf('month')] as DT,
+        本季: [quarter_start, quarter_end] as DT,
+        今年: [start(m).startOf('year'), end(m).endOf('year')] as DT,
         近一年: [start(m).add(-1, 'year').add(1, 'day'), end(m)] as DT,
         年初至今: [start(m).startOf('year'), end(m)] as DT,
     }
+}
+
+export function presets_range() {
+    const rages = getMomentRange()
+    return listify(rages, (label, value) => ({ label, value }))
 }
 export function presets_date() {
 
